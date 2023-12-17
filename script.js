53b60f49a1174f533d09ad0551fd8a37230912f7
Shadix95 <103731216+Shadix95@users.noreply.github.com>
2023-12-17 23:45:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ var closeBtn = document.getElementById("closeBtn");
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
+/* Set the left position of the side navigation to 0 */
 function openNav() {
   sidenav.classList.add("active");
+  sidenav.style.right = 0;
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
   sidenav.classList.remove("active");
+  sidenav.style.right = "-250px"; /* Réglez la position initiale sur la droite */
 }
