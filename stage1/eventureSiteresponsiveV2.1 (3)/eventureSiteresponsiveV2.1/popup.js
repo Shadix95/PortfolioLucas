9e4b443d89f496fb16c78881e29f2eb21ded9f14
Shadix95 <103731216+Shadix95@users.noreply.github.com>
2023-12-17 23:42:39 +0100

    Create popup.js

---
+++ stage1/eventureSiteresponsiveV2.1 (3)/eventureSiteresponsiveV2.1/popup.js
@@ -0,0 +1,9 @@
+function popUp(){
+    if(confirm("le topic va être archivé")){
+        alert("le topic est archivé");
+        txt = "Le topic est archivé";
+    } else {
+        alert("le topic c'est pas archivé");
+        txt = "Le topic n'est pas archivé"
+    }
+}
