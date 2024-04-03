db718186ef70705894328d5487745bc1ad4358ae
Shadix95 <103731216+Shadix95@users.noreply.github.com>
2024-04-04 00:42:14 +0200

    MODIF

--- stage2/afficher la date sur linux = date.sh
+++ stage2/afficher la date sur linux = date.sh
@@ -1,9 +1,9 @@
+
 afficher la date sur linux = date
 
-date-expiration = 2024/02/18
-date-actuel = 2024/01/22
+date-expiration = 2024/02/18   #ex
+date-actuel = 2024/01/22       #ex
 
- 
 
 EEEE/BB/AA
 AA max 30 puis AA reviens a 1
@@ -11,26 +11,22 @@ BB max 12 puis BB reviens a 1
 EEEE +1 chaque fois que BB et AA passe a 1
 
 si date-expiration - date-actuel >= 30
-    alors envoyer " Il vous reste 1 mois avant que la clée expire "
+    alors envoyer " Il vous reste 1 mois avant que le fichier expire "
 sinon
     rien faire
 
  
 
 
-Script//
+#Script 1//
 
-#! /bin/bash
 
 $%m =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-
 $ date-actuel = $(date +"%m-%d-%Y")
-
 $ echo $cdate
 
 RUNAT="10:00"
-
 
 while [ $%m==30 ]
 do
@@ -52,33 +48,29 @@ do
 done
 
  
-
 if date-expiration - date-actuel >= 30 then
-    echo "Memory usage is above threshold: $memory" | mail -s "La clé va bientot expirer" zina.boudjemaa@bnpparibas-pf.com
-
-
-
+    echo "Memory usage is above threshold: $memory" | mail -s "Le fichier va bientot expirer" zina.boudjemaa@bnpparibas-pf.com
 
 
 #Script 2//
 
 
-$%m =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
- 
 $date_actuel = $(date +"%m-%d-%Y")
-$ echo $cdate
+$ echo cdate
 
- 
 
 if [$date_actuel - $date_expiration < 1]
 then  
+
     source C:\Users\f56628\Downloads\stage2\CONTROL_FILES\bin\logverifPY.ksh
-    echo "La clé va bientot expiré" | bash /applis/shared/bin/mailxpf.sh --starttls --netrcFile /home/pfmgr/netrc --from ema.pf.dhfc-rundatait@bnpparibas.com --subject "hello" --to sebastien.pot@bnpparibas-pf.com --timeoutConnect 21 --collectorName "smtp-appli-ema.group.echonet.net.intra" --collectorPort 25 --mimeType HTML --data -   #lancement de script loverifPY.ksh pour envoie du mail
+    echo "La clé va bientot expiré" | bash /applis/shared/bin/mailxpf.sh --starttls --netrcFile /home/pfmgr/netrc
+    --from ema.pf.dhfc-rundatait@bnpparibas.com --subject "hello" --to sebastien.pot@bnpparibas-pf.com 
+    --timeoutConnect 21 --collectorName "smtp-appli-ema.group.echonet.net.intra" --collectorPort 25 
+    --mimeType HTML --data -   #lancement de script loverifPY.ksh pour envoie du mail
+
 fi
 
  
-
 while [ $m=[] ]
 do
     DATE=`/bin/date +%Y-%m-%d`
